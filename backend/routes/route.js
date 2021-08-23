7f9e26445a3ff51563ddfa759e3a2d0fdee94e15
yatin-07 <nish07ravenclaw@gmail.com>
2021-08-23 14:08:03 +0530

    backend

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,18 +1,19 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const Signin = require('../models/singin-schema');
-router.post('/signin', (req,res) => {
-    const signUser = new Signin({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+const Signin = require("../models/singin-schema");
+router.post("/signin", (req, res) => {
+  const signUser = new Signin({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  signUser
+    .save()
+    .then((result) => {
+      res.json(result);
     })
-    signUser.save()
-    .then((data) =>{
-        res.json(data)
-    })
-    .catch((error) => {
-        res.json(error)
-    })
-})
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
