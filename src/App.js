728c87a20d768c0b3da39e583ca6f3cfb9523b81
yatin-07 <nish07ravenclaw@gmail.com>
2021-08-23 19:17:03 +0530

    register form created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Navigation from "./componenet/navigation";
 import Signin from "./componenet/signin";
-
+import Register from "./componenet/register";
 class App extends Component {
 constructor() {
   super();
@@ -15,14 +15,22 @@ constructor() {
   }
 }
 
-onRouteChange = () => {
-this.setState({route: 'home'});
+onRouteChange = (route) => {
+this.setState({route: route});
 }
 
   render(){
     return(
       <div className="App">
+        <Navigation onRouteChange={this.onRouteChange} />
+        {
+          this.state.route === 'signin'
+          ?
             <Signin onRouteChange={this.onRouteChange}/>
+            :
+            <Register onRouteChange={this.onRouteChange}/>
+        }
+            
       </div>
       );
   }
--- src/componenet/register.js
+++ src/componenet/register.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 
-const Signin = ({onRouteChange}) => {
+const Register = ({onRouteChange}) => {
 return(
   <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadaow-5 center">
   <main className="pa4 black-80">
   <form className="measure center">
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-      <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+      <legend className="f4 fw6 ph0 mh0">Register</legend>
+      <div className="mt3">
+        <label className="db fw6 lh-copy f6" for="name">Name</label>
+        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+      </div>
       <div className="mt3">
         <label className="db fw6 lh-copy f6" for="email-address">Email</label>
         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -24,11 +28,7 @@ return(
        value="Sign in" />
     </div>
     <div className="lh-copy mt3">
-    <div class="lh-copy mt3">
-      <p
-      onClick={() => onRouteChange('register')} 
-       class="f6 link dim black db">register</p>
-    </div>
+   
      
     </div>
   </form>
@@ -38,4 +38,4 @@ return(
 );
 }
 
-export default Signin;
+export default Register;
